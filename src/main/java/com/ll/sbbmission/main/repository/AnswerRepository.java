eae6de5b499f92685f3835c8a9b470bad7020040
Bang210 <bangvelope@gmail.com>
2023-11-22 00:42:31 +0900

    2_5

---
+++ src/main/java/com/ll/sbbmission/main/repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.ll.sbbmission.main.repository;
+
+import com.ll.sbbmission.main.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
