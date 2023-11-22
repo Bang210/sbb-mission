6fb6f82c2713862eac2a6fda59d64a214c35169b
Bang210 <bangvelope@gmail.com>
2023-11-22 22:00:19 +0900

    2_15

---
+++ src/main/java/com/ll/sbbmission/domain/answer/AnswerForm.java
@@ -0,0 +1,10 @@
+package com.ll.sbbmission.domain.answer;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class AnswerForm {
+    @NotEmpty(message = "내용을 입력해주세요.")
+    private String content;
+}
